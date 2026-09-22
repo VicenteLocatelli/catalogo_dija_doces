538c4f7df8a88b5d34314332c4b60b7f55867ccd
VicenteLocatelli <vicente.locatelli@hotmail.com>
2026-09-21 22:33:13 -0300

    Correção NUMERO_W

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Número de WhatsApp do seu sogro (Ex: 55 + DDD + Número sem espaços)
-const NUMERO_WHATSAPP = "55819919639777"; 
+const NUMERO_WHATSAPP = "5581919639777"; 
 
 // Lista completa de produtos (atualizada com os novos prints do catálogo)
 const produtos = [
